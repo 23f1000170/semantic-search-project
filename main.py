411657df16e07d80abf4c4effc8968473165d516
Sristy Gupta <sristyvidhyarthi@gmail.com>
2026-02-15 19:33:03 +0530

    Enable CORS for external access

--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import time
 import pickle
 from documents import documents
 from embeddings import get_embedding, vector_search, rerank
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
